b7a031d0fdbdbeafa6774e876aa2c13d442e773b
Casey Bee <caseye.baker@gmail.com>
2019-04-29 15:48:56 -0700

    selection_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,18 +5,37 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
+        
+        # loop through RHS of cur_index
+        for j in range(cur_index, len(arr)): # second to last for above loop, not this one!
+            rhs_index = j
+            # if the value of the smallest index is more than value of rhs_index
+            # smallest index = rhs_index
 
-
+            print(arr[smallest_index], " > ", arr[rhs_index], "?")
+            if arr[smallest_index] > arr[rhs_index]:
+                smallest_index = rhs_index
+                print("Yes! New smallest index:", smallest_index, ": ", arr[smallest_index])
+            else:
+                print("Nope, smallest index is still:", smallest_index, ": ", arr[smallest_index])
 
         # TO-DO: swap
+        # now I have the smallest_index and cur_index
+        # placeholder? value of smallest_index = cur_index and cur_index = smallest_index
+        # placeholder holds smallest_index value
+        placeholder = arr[smallest_index]
+        print("placeholder", placeholder, "cur_index", cur_index, arr[cur_index])
+        # smallest_index gets cur_index value
+        # cur_index value gets smallest_index value FROM placeholder
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = placeholder
 
-
-
-
+    print(arr)
     return arr
 
+testArr = [3, 2, 4, 0]
+selection_sort(testArr)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
