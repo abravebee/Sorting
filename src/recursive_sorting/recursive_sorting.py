aee0c4216c71685e0ed7c7447a39e334c76b53fd
Casey Bee <caseye.baker@gmail.com>
2019-05-07 15:06:15 -0700

    adds pseudocode for merge_sort() and merge()

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,16 +1,29 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
+    print(elements)
     merged_arr = [0] * elements
     # TO-DO
-    
+    # loop thru elements
+    # compare arrA values to arrB values to find smallest
+    # replace value on merged arr? why not push onto empty arr?
+    # use incremental variables to track array indices
+    # i for merged arr
+    # j and k for arrs
+    # j++ or k++ respectively when value IS smaller
     return merged_arr
 
+print(merge([1], [2]))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-
+    # if length is > 1
+        # split using [mid:] = lhs and [:mid] = rhs
+        # mid = length/2
+    # call recursively to split
+    # call merge(lhs,rhs)
+    # else:
     return arr
 
 
